47c3d3ce65652e55c639fece4d703c69a6e88126
guille <guillermoneculqueo@gmail.com>
2022-10-20 22:58:01 -0300

    probando sumar cosas

--- main.js
+++ main.js
@@ -5,19 +5,30 @@ const funko4=1000;
 const funko5=700;
 const iva=0.16;
 
+// funcion normal de bienvenida
 function bienvenido() {
     let ingresar = prompt("deseas ver mi web ??? si / no").toLowerCase();
     (ingresar === "si" && (ingresar != "" || ingresar != null))?
         alert("bienvenido") : (alert("adios") && console.log("ERROR"))
 }
 
+//funcion flecha para el menu
+const eleccion = () => {
+    return ("1- kaonash : "+funko1+"\n2 - kaonashi duo : "+funko2+"\n3- calafuego : "+funko3+"\n4) gorrito: "+funko3+"\ncual comprara: ");
+}
+
+const iva_total = (total) => {
+    total * iva + total;
+}
+
+// +++ TESTEAMOS  +++
+//lamamos a la funcion bienvenido
 bienvenido();
 
 if (confirm("si desea continuar debe abrir la consola.")) {
 
     while (confirm("ver menu?") == true) {
-        let prod = prompt("1- kaonash : "+funko1+"\n2 - kaonashi duo : "+funko2+"\n3- calafuego : "+funko3+"\n4) gorrito: "+funko3+"\ncual comprara: ");
-
+        let prod = prompt(""+eleccion());
     
         if (confirm("confirma la compra?")) {
             console.log("gracias por comprar!!! ") 
@@ -31,16 +42,3 @@ if (confirm("si desea continuar debe abrir la consola.")) {
 } else{
     console.log("CHAU");
 }
-
-
-
-
-const eleccion = () => {
-    console.log("cual de los funcos te gusto: [1..6]: ");
-}
-
-function resta(a, b) {
-    return a - b;
-}
-
-eleccion();
