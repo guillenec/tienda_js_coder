ae35c79811f03d452bb68192e5324053b05b862f
guille <guillermoneculqueo@gmail.com>
2022-11-07 19:51:30 -0300

    dom 1

---
+++ js/doom.js
@@ -0,0 +1,63 @@
+console.log(document.body);
+
+const depura = (texto) =>{
+    let dep = "\n---- Depurador => "
+    console.log(`${dep} ${texto} ---- \n`)
+}
+
+depura("tarjeta simple");
+console.log(document.querySelector(".targeta_simple"));
+
+depura("todas las tarjetas");
+console.log(document.querySelectorAll(".targeta_simple"));
+
+depura("guardo coleccion de tarjetas en array y mostramos 1°:");
+let tarjetas = document.querySelectorAll(".targeta_simple");
+console.log(tarjetas[0]);
+
+depura("recorremos todo el arreglo foreach");
+tarjetas.forEach(element => {
+    console.log(element);
+});
+
+depura("usamos forof");
+for (const iterator of tarjetas) {
+    console.log(iterator);
+}
+
+depura("accedemos a la lista de clase classNme classList");
+let tarCategoria = document.querySelector("#top_prod");
+console.log(tarCategoria.className);    //devuelve string
+console.log(tarCategoria.classList);    //devuelve arreglo
+
+depura("añadimos una clase al elemento: .add");
+tarCategoria.classList.add("YamshaPuar");
+console.log(tarCategoria.classList);
+
+//tarCategoria.className("YamshaPuar"); //No se recomienda ya que borra las clases que ya posea
+
+depura("quitamos la clase al elemento: .remove");
+tarCategoria.classList.remove("YamshaPuar");
+console.log(tarCategoria.classList);
+
+depura("accedemos al html con innerHTML: \nDevuelve la estructura entera, HTML correspondiente.");
+console.log(tarCategoria.innerHTML);
+
+depura("accedemos al html con innerText: \nDevuelve solo el texto correspondiente.");
+console.log(tarCategoria.innerText);
+
+depura("cambio texto con innerText");
+//let tituloCategorias = document.querySelectorAll(".categorias");
+
+tarjetas[0].innerHTML = "hola mundo 1";
+
+
+tarjetas[1].innerText = "hola mundo 2";
+
+
+tarjetas[0].innerHTML = `<img src="https://res.cloudinary.com/dpiwmbsog/image/upload/v1667102369/imgs/haloween/funkos/pelis/jon_week3_axwmnn.png" alt="jon week">
+<div class="textos_target">
+    <a class="title_targ">movies</a>
+    <p>Funkos de Peliculas, series como House Of The Dragons o Mandalorian. </p>
+</div>`;
+
--- js/toggle.js
+++ js/toggle.js
@@ -1,7 +1,16 @@
-const nav_menu = document.querySelector("#toggle");
+const nav_menu = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+
 
 nav_menu.addEventListener("click", () => {
     nav_menu.classList.toggle("active");
+    menu.classList.toggle("active");
 })
+
+window.addEventListener('click', e =>{
+    if(menu.classList.contains('active') && e.target != menu && e.target != nav_menu){
+        nav_menu.classList.toggle('active');
+    }
+}) 
 
 
